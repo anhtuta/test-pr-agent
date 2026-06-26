1d350c17558abcb383722c61a6849234666ae4b9
Tu Ta <tu.ta@zen8labs.com>
2026-06-26 18:07:22 +0700

    Another test

--- Java8Demo/src/hello/stupidcode/Demo1.java
+++ Java8Demo/src/hello/stupidcode/Demo1.java
@@ -1,11 +1,17 @@
 package hello.stupidcode;
 
 public class Demo1 {
+    private String name;
+    private String address;
     static String getMyName() {
         return "John Doe";
+    }
+    static String Say_something_to_me() {
+        return "Hello " + getMyName() + "!" + "This is a stupid code" + "and I am a stupid person" + ", hu hu hu!!!";
     }
 
     public static void main(String[] args) {
         System.out.println("Hello " + get_my_Name() + "!");
+        System.out.println(Say_something_to_me());
     }
 }
